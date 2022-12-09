9632e085445324e42d47a2d659a8260302de1bc4
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2022-12-09 04:52:16 -0800

    Create deployment, service, and ingress

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/txn2/txeh v1.3.0
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"strconv"
 
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
+	ingress2 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func CreateApp(clientset *kubernetes.Clientset, name string, image string, port int32) {
+func CreateApp(clientset *kubernetes.Clientset, name string, image string, portNo string, domain string) {
+	port, err6 := strconv.Atoi(portNo)
+	if err6 != nil {
+		fmt.Println("Error while type convertion")
+	}
 	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -20,13 +27,13 @@ func CreateApp(clientset *kubernetes.Clientset, name string, image string, port 
 		Spec: appsv1.DeploymentSpec{
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": "demo",
+					"app": name,
 				},
 			},
 			Template: apiv1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": "demo",
+						"app": name,
 					},
 				},
 				Spec: apiv1.PodSpec{
@@ -38,7 +45,7 @@ func CreateApp(clientset *kubernetes.Clientset, name string, image string, port 
 								{
 									Name:          "http",
 									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: port,
+									ContainerPort: int32(port),
 								},
 							},
 						},
@@ -48,19 +55,21 @@ func CreateApp(clientset *kubernetes.Clientset, name string, image string, port 
 		},
 	}
 
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	deploymentsClient := clientset.AppsV1().Deployments("dev")
 
 	fmt.Println("Creating deployment...")
 	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+	CreateService(clientset, name, port, domain)
 }
 
 func GetApps(clientset *kubernetes.Clientset) {
 
-	deployments, err4 := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
+	deployments, err4 := clientset.AppsV1().Deployments("dev").List(context.Background(), metav1.ListOptions{})
 	if err4 != nil {
 		fmt.Println("Error in creating deployments")
 	}
@@ -69,63 +78,126 @@ func GetApps(clientset *kubernetes.Clientset) {
 	}
 }
 
-// func createService() {
-// 	kubeconfig := flag.String("kubeconfig", "C:\\Users\\WSO2\\.kube\\config", "location to your cube config files")
-// 	config, err1 := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-// 	if err1 != nil {
-// 		//handle error
-// 		fmt.Println("Error in creating config")
-// 	}
+func GetServices(clientset *kubernetes.Clientset) {
 
-// 	clientset, err2 := kubernetes.NewForConfig(config)
-// 	if err2 != nil {
-// 		fmt.Println("Error in creating clientset")
-// 	}
+	services, err4 := clientset.CoreV1().Services("dev").List(context.Background(), metav1.ListOptions{})
+	if err4 != nil {
+		fmt.Println("Error while getting service")
+	}
+	for _, service := range services.Items {
+		fmt.Println(service.Name)
+	}
+}
 
-// 	clientset.CoreV1().Services("kube-system").Create(&apiv1.Service{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      "Test service",
-// 			Namespace: "kube-system",
-// 			Labels: map[string]string{
-// 				"k8s-app": "kube-controller-manager",
-// 			},
-// 		},
-// 		Spec: apiv1.ServiceSpec{
-// 			Ports:     nil,
-// 			Selector:  nil,
-// 			ClusterIP: "",
-// 		},
-// 	})
+func CreateService(clientset *kubernetes.Clientset, name string, port int, domain string) {
+	Service := &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name + "-service",
+			Namespace: "dev",
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: apiv1.ServiceSpec{
+			Selector: map[string]string{
+				"app": name,
+			},
+			Ports: []apiv1.ServicePort{
+				{
+					Protocol:   apiv1.ProtocolTCP,
+					Port:       int32(port),
+					TargetPort: intstr.FromInt(port),
+				},
+			},
+		},
+	}
 
-// 	// service := apiv1.Service{
-// 	// 	ObjectMeta: metav1.ObjectMeta{
-// 	// 		Name:      "myservice",
-// 	// 		Namespace: "default",
-// 	// 		Labels: map[string]string{
-// 	// 			"app": "myapp",
-// 	// 		},
-// 	// 	},
-// 	// 	Spec: apiv1.ServiceSpec{
-// 	// 		Ports:     nil,
-// 	// 		Selector:  nil,
-// 	// 		ClusterIP: "",
-// 	// 	},
-// 	// }
+	fmt.Println("Creating service...")
+	result, err := clientset.CoreV1().Services("dev").Create(context.TODO(), Service, metav1.CreateOptions{})
 
-// 	// serviceClient := clientset.AppsV1().Services(apiv1.NamespaceDefault)
+	if err != nil {
+		fmt.Println("Error while getting service")
+	}
 
-// 	// // Create Service
-// 	// fmt.Println("Creating service...")
-// 	// result, err := serviceClient.Create(service)
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
-// }
+	var service_name string = result.GetObjectMeta().GetName()
+	fmt.Printf("Created service %q.\n", result.GetObjectMeta().GetName())
+	CreateIngress(clientset, service_name, domain, port)
+}
+
+func GetIngresses(clientset *kubernetes.Clientset) {
+	ingressList, err := clientset.NetworkingV1().Ingresses("dev").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("Error while getting ingresses")
+	}
+	ingressCtrls := ingressList.Items
+	if len(ingressCtrls) > 0 {
+		for _, ingress := range ingressCtrls {
+			fmt.Printf("ingress %s exists in namespace %s\n", ingress.Name, ingress.Namespace)
+		}
+	} else {
+		fmt.Println("no ingress found")
+	}
+}
+
+func CreateIngress(clientset *kubernetes.Clientset, service string, domain string, port int) {
+	var str string = "Prefix"
+	ingressService := &ingress2.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      service + "-ingress",
+			Namespace: "dev",
+		},
+		Spec: ingress2.IngressSpec{
+			Rules: []ingress2.IngressRule{
+				{
+					Host: domain,
+					IngressRuleValue: ingress2.IngressRuleValue{
+						HTTP: &ingress2.HTTPIngressRuleValue{
+							Paths: []ingress2.HTTPIngressPath{
+								{
+									Path:     "/",
+									PathType: (*ingress2.PathType)(&str),
+									Backend: ingress2.IngressBackend{
+										Service: &ingress2.IngressServiceBackend{
+											Name: service,
+											Port: ingress2.ServiceBackendPort{
+												Number: int32(port),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			// TLS: []ingress2.IngressTLS{
+			// 	{
+			// 		Hosts: []string{
+			// 			"demo.mlopshub.com",
+			// 		},
+			// 		SecretName: "hello-app-tls",
+			// 	},
+			// },
+		},
+	}
+
+	fmt.Println("Creating Ingress...")
+	result, err := clientset.NetworkingV1().Ingresses("dev").Create(context.TODO(), ingressService, metav1.CreateOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+}
 
 func main() {
 
-	jobName := flag.String("jobname", "", "The name of the job")
-	containerImage := flag.String("image", "", "Name of the container image")
+	Name := flag.String("name", "", "The name of the app")
+	containerImage := flag.String("image", "", "Name of the image")
+	port := flag.String("port", "", "Port number")
+	domain := flag.String("domain", "", "Domain name")
+
+	//DNS := flag.String("dns", "", "Domain name")
 
 	flag.Parse()
 
@@ -141,11 +213,36 @@ func main() {
 		fmt.Println("Error in creating clientset")
 	}
 
-	if *jobName != "" && *containerImage != "" {
-		CreateApp(clientset, *jobName, *containerImage, 80)
+	flag.Parse()
+
+	if *Name != "" && *containerImage != "" && *port != "" && *domain != "" {
+		// PortNo, err6 := strconv.Atoi(*Port)
+		// if err6 != nil {
+		// 	fmt.Println("Error while type convertion")
+		// }
+		CreateApp(clientset, *Name, *containerImage, *port, *domain)
+		// hosts, err := txeh.NewHostsDefault()
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		// hosts.AddHost("127.0.0.1", *domain)
+		// hfData := hosts.RenderHostsFile()
+
+		// if you like to see what the outcome will
+		// look like
+		// fmt.Println(hfData)
+
+		// hosts.Save()
 
 	} else {
+		//CreateApp(clientset, "demo", "gcr.io/google-samples/hello-app:2.0", 80, "example.com")
+		//CreateIngress(clientset, "demo-service", "example.com", 80)
+		//CreateService(clientset, "demo", 80, "example.com")
 		GetApps(clientset)
+		//GetIngresses(clientset)
+		//GetServices(clientset)
+		//CreateService(clientset)
 
 	}
 
@@ -155,43 +252,4 @@ func main() {
 
 	//createService()
 
-	// kubeconfig := flag.String("kubeconfig", "C:\\Users\\WSO2\\.kube\\config", "location to your cube config files")
-	// config, err1 := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	// if err1 != nil {
-	// 	//handle error
-	// 	fmt.Println("Error in creating config")
-	// }
-
-	// clientset, err2 := kubernetes.NewForConfig(config)
-	// if err2 != nil {
-	// 	fmt.Println("Error in creating clientset")
-	// }
-
-	// ctx := context.Background()
-	// pods, err3 := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
-	// if err3 != nil {
-	// 	fmt.Println("Error in creating pods")
-	// }
-
-	// for _, pod := range pods.Items {
-	// 	fmt.Printf("%s", pod.Name)
-	// }
-
-	// deployments, err4 := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
-	// if err4 != nil {
-	// 	fmt.Println("Error in creating deployments")
-	// }
-	// for _, deployment := range deployments.Items {
-	// 	fmt.Printf("%s", deployment.Name)
-	// }
-
-	//deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-
-	// Create Deployment
-	// fmt.Println("Creating deployment...")
-	// result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
 }
